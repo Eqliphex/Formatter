a84eb3b49b4f8a8e568757f55052dca633156374
Patrick Wegener Meyer <tbt_paddik@hotmail.com>
2018-05-25 10:16:35 +0200

    Add files via upload

---
+++ formatter.js
@@ -0,0 +1,2 @@
+const TOTAL_ADVERTS_CSS_SELECTOR = '#rightcol > div.h1-prefix > nobr > table > tbody > tr > td > span > nobr';
+const TOTAL_ADVERTS_XPATH_SELECTOR = '//*[@id="rightcol"]/div[1]/nobr/table/tbody/tr/td/span/nobr';
